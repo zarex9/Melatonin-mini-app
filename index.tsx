88f2f027ad832ddc04a193dd98596308a0e1d152
Zarex <118372151+zarex9@users.noreply.github.com>
2026-01-10 09:25:10 +0530

    Fix typo in error message for root element

--- index.tsx
+++ index.tsx
@@ -5,7 +5,7 @@ import AppWrapper from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error("Could not find root element to ount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
